cf2034b0df996d812d6f3c8f90891a5057760daa
RusaDrako <f182@rambler.ru>
2023-11-27 12:26:13 +0300

    Добавление методов Response->getArray() и Response->getJSON()

--- src/Response.php
+++ src/Response.php
@@ -1,12 +1,36 @@
 <?php
 namespace RusaDrako\sberbank_ecom_client;
 
+use mysql_xdevapi\Exception;
+
 /**
  * Объект ответа
  */
 class Response extends Item{
+
+	/** @var string */
+	private $_jsonOptions;
+
 	/** */
-	public function __construct(array $options){
-		$this->options = $options;
+	public function __construct(string $jsonOptions){
+		$this->_jsonOptions = $jsonOptions;
+		parent::__construct(\json_decode($this->_jsonOptions, true));
 	}
+
+	/**
+	 * Возвращает результат запроса в формате JSON
+	 * @return string
+	 */
+	public function getJSON() {
+		return $this->_jsonOptions;
+	}
+
+	/**
+	 * Возвращает результат запроса в формате Array
+	 * @return array
+	 */
+	public function getArray() {
+		return $this->_options;
+	}
+
 }
---
+++ tests/Response__Test.php
@@ -0,0 +1,44 @@
+<?php
+namespace RusaDrako\sberbank_ecom_client\Tests;
+
+use PHPUnit\Framework\TestCase;
+use RusaDrako\sberbank_ecom_client\Response;
+
+require_once(__DIR__ . '/../src/autoload.php');
+
+/**
+ * @author Петухов Леонид <f182@rambler.ru>
+ */
+class Response__Test extends TestCase {
+
+	private $className = Response::class;
+
+	/** Вызывается перед каждым запуском тестового метода */
+	protected function setUp() : void {}
+
+	/** Вызывается после каждого запуска тестового метода */
+	protected function tearDown() : void {}
+
+	/** Проверка получения параметров объекта */
+	public function test__options() {
+		$json = '{"errorCode":5,"errorMessage":"Доступ запрещен"}';
+		$response = new $this->className($json);
+		$this->assertEquals(5, $response->errorCode);
+		$this->assertEquals("Доступ запрещен", $response->errorMessage);
+	}
+
+	/** Метод getJSON() */
+	public function test__getJSON() {
+		$json = '{"errorCode":5,"errorMessage":"Доступ запрещен"}';
+		$response = new $this->className($json);
+		$this->assertEquals($json, $response->getJSON());
+	}
+
+	/** Метод getArray() */
+	public function test__getArray() {
+		$json = '{"errorCode":5,"errorMessage":"Доступ запрещен"}';
+		$response = new $this->className($json);
+		$this->assertEquals(['errorCode' => 5, 'errorMessage' => 'Доступ запрещен'], $response->getArray());
+	}
+
+}
